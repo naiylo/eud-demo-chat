dcbd568602814d676b0c22cc4e15271dd94be68d
Oskar Schiedewitz <schiedewitz1@uni-potsdam.de>
2026-01-20 15:04:00 +0100

    remove old versioned widgets

--- src/widgets/registry.ts
+++ src/widgets/registry.ts
@@ -1,10 +1,6 @@
 import type { AnyWidgetDefinition } from "./types";
-import { StandardPoll } from "./StandardPoll";
-import { PollBrokenDeleteVoteFunction } from "./PollBrokenDeleteVoteFunction";
 import { examplepoll } from "./examplepoll";
 
 export const widgetRegistry: AnyWidgetDefinition[] = [
-  StandardPoll,
-  PollBrokenDeleteVoteFunction,
   examplepoll,
 ];
